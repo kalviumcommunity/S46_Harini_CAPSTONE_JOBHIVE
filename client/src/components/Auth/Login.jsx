e46f58ea70239ecaa953a5c4ea9dd3d438c44bad
harini18122005 <144237501+harini18122005@users.noreply.github.com>
2025-01-17 22:05:40 +0530

    done changes in frontend server

--- client/src/components/Auth/Login.jsx
+++ client/src/components/Auth/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/user/login",
+        "https://s46-harini-capstone-jobhive-4.onrender.com/api/v1/user/login",
         { email, password, role },
         {
           headers: {
