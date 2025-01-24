bb62ed501c2d37ea96a0a697f551dc129ab8a459
harini18122005 <144237501+harini18122005@users.noreply.github.com>
2025-01-24 17:14:19 +0530

    final deployement

--- client/src/components/Job/Jobs.jsx
+++ client/src/components/Job/Jobs.jsx
@@ -10,7 +10,7 @@ const Jobs = () => {
   useEffect(() => {
     try {
       axios
-        .get("http://localhost:4000/api/v1/job/getall", {
+        .get("https://s46-harini-capstone-jobhive.onrender.com/api/v1/job/getall", {
           headers: {
             "Content-Type": "application/json",
             "Authorization":token,
